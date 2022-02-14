4c309b5211fbdd7a8ad545c95fd224daa7ce77fa
xDruid <savelyev.eg@gmail.com>
2022-02-14 17:07:19 +0700

    repare mailservice

--- app/mailservice/routes.py
+++ app/mailservice/routes.py
@@ -27,8 +27,8 @@ def send_response_email(app, company, sender, sender_email, sender_phone, respon
 def send_verification_email(app, destination, key):
     with app.app_context():
         msg = Message(subject=f'Поддверждение отправки отзыва ИЛТССД', sender=app.config['MAIL_USERNAME'], recipients=[destination])
-        msg.body = f'Для подтверждения отправки Отзыва в ИЛТССД перейдите по ссылке http://xn--d1ahjyae.xn--p1ai/confirm?uuid={key}'
-        msg.html = f'<h3>Для подтверждения отправки Отзыва в ИЛТССД перейдите по ссылке</h3><p><a href="http://xn--d1ahjyae.xn--p1ai/confirm?uuid={key}">Подтвердить отправку</a></p>'
+        msg.body = f'Для подтверждения отправки Отзыва в ИЛТССД перейдите по ссылке xn--d1ahjyae.xn--p1ai/confirm?uuid={key}'
+        msg.html = f'<h3>Для подтверждения отправки Отзыва в ИЛТССД перейдите по ссылке</h3><p><a href="xn--d1ahjyae.xn--p1ai/confirm?uuid={key}">Подтвердить отправку</a></p>'
         try:
             mail.send(msg)
         except:
